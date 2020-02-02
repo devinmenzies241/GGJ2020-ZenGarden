26cd5230887a8f7d1b9700eb3ccf9d52c7905c1c
Womp Rat <devinmenzies@gmail.com>
2020-02-02 12:46:16 -0700

    Audio assets

---
+++ UnityProject/ZenGarden/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,79 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_2_UI_CLICK_1 = 3077739996U;
+        static const AkUniqueID PLAY_ACHIEVEMENT_UNLOCK = 510317686U;
+        static const AkUniqueID PLAY_BRIGHT_CLICK = 330433247U;
+        static const AkUniqueID PLAY_DIRT_PICK_01 = 3428451851U;
+        static const AkUniqueID PLAY_DIRT_PICK_02 = 3428451848U;
+        static const AkUniqueID PLAY_DIRT_PICK_04 = 3428451854U;
+        static const AkUniqueID PLAY_DIRT_PICK_05 = 3428451855U;
+        static const AkUniqueID PLAY_ENTER_GAME = 1823049125U;
+        static const AkUniqueID PLAY_ERR_MISCLICK = 2450801821U;
+        static const AkUniqueID PLAY_FAIL_SOUND = 2311302334U;
+        static const AkUniqueID PLAY_FOLIAGE_01 = 258578655U;
+        static const AkUniqueID PLAY_FOLIAGE_02 = 258578652U;
+        static const AkUniqueID PLAY_FOLIAGE_03 = 258578653U;
+        static const AkUniqueID PLAY_FOLIAGE_04 = 258578650U;
+        static const AkUniqueID PLAY_GAME_MENU_MUSIC = 202855082U;
+        static const AkUniqueID PLAY_GARDEN_AMBIENCE = 1551358484U;
+        static const AkUniqueID PLAY_PULSE_WAITING = 519352333U;
+        static const AkUniqueID PLAY_QUESTION_UNSURE = 2611228715U;
+        static const AkUniqueID PLAY_RACOON_01 = 4200519586U;
+        static const AkUniqueID PLAY_RACOON_02 = 4200519585U;
+        static const AkUniqueID PLAY_RACOON_03 = 4200519584U;
+        static const AkUniqueID PLAY_ROCK_01 = 2997304641U;
+        static const AkUniqueID PLAY_ROCK_02 = 2997304642U;
+        static const AkUniqueID PLAY_UI_04 = 1329976963U;
+        static const AkUniqueID PLAY_UI_05 = 1329976962U;
+        static const AkUniqueID PLAY_UI_06 = 1329976961U;
+        static const AkUniqueID PLAY_UI_07 = 1329976960U;
+        static const AkUniqueID PLAY_UI_08 = 1329976975U;
+        static const AkUniqueID PLAY_UI_09 = 1329976974U;
+        static const AkUniqueID PLAY_UI_10 = 1313199380U;
+        static const AkUniqueID PLAY_UI_11 = 1313199381U;
+        static const AkUniqueID PLAY_UI_12 = 1313199382U;
+        static const AkUniqueID PLAY_UI_13 = 1313199383U;
+        static const AkUniqueID PLAY_UI_14 = 1313199376U;
+        static const AkUniqueID PLAY_UI_15 = 1313199377U;
+        static const AkUniqueID PLAY_UI_16 = 1313199378U;
+        static const AkUniqueID PLAY_UI_CLICK_02 = 3155308026U;
+        static const AkUniqueID PLAY_UI_CLICK_03 = 3155308027U;
+        static const AkUniqueID PLAY_UP_SHIFT = 3570728296U;
+        static const AkUniqueID PLAY_WRITING = 2714409814U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID PUZZLE = 1780448749U;
+        static const AkUniqueID STORY = 1686739444U;
+        static const AkUniqueID TITLE_SCREEN = 3853285476U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
